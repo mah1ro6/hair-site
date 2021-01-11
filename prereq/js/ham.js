3d0c83add96221ed30447bb4e9cb6752962f7bb5
mahiro/miyazaki <75361336+miyama12style@users.noreply.github.com>
2021-01-12 01:35:22 +0900

    Add files via upload

--- prereq/js/ham.js
+++ prereq/js/ham.js
@@ -2,12 +2,20 @@ function toggleNav() {
   const body = document.body;
   const hum = document.getElementById('target');
   const black = document.getElementById('js-black');
+  const menus = document.getElementsByClassName('menu1');
 
-  hum.addEventListener('click', function() {
+  hum.addEventListener('click', () => {
     body.classList.toggle('nav-open');
   });
-  black.addEventListener('click', function() {
+  black.addEventListener('click', () => {
     body.classList.remove('nav-open');
   });
+  for(let i= 0; i < menus.length; i++){
+    menu = menus[i] ;
+    menu.addEventListener('click', () => {
+      body.classList.remove('nav-open');
+    })
+  }
 } 
 toggleNav();
+
